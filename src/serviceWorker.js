55ced1a1b70ab3cfdbbf201a41bc8f2ae9fc5ce8
Billy Campbell <wcampbell91@gmail.com>
2020-09-10 20:40:55 -0500

    added data to database

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-parens */
+/* eslint-disable comma-dangle */
+/* eslint-disable operator-linebreak */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
