21041e6395ff3d066136a5fa9b387c9ea7c594b3
Billy Campbell <wcampbell91@gmail.com>
2020-09-12 12:46:55 -0500

    added box-shadow to album cards

--- src/components/shared/AlbumCards/AlbumCards.js
+++ src/components/shared/AlbumCards/AlbumCards.js
@@ -6,11 +6,11 @@ const AlbumCards = (props) => {
   const { record } = props;
 
   return (
-    <div class="card">
-      <img class="card-img-top" src={record.coverImage} alt="Album Cover" />
-      <div class="card-body">
-        <h3 class="card-title">{record.artist}</h3>
-        <h4 class="card-title">{record.album}</h4>
+    <div className="card">
+      <img className="card-img-top" src={record.coverImage} alt="Album Cover" />
+      <div className="card-body">
+        <h3 className="card-title">{record.artist}</h3>
+        <h4 className="card-title album-title">{record.album}</h4>
         <button className="btn btn-primary">View Album</button>
       </div>
     </div>
