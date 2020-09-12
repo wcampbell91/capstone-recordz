716b46ba0300378f89b6f51d980e71caf66b43f7
Billy Campbell <wcampbell91@gmail.com>
2020-09-12 12:39:49 -0500

    READ accomplished

---
+++ src/helpers/data/collectionData.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+import utils from '../utils';
+
+const baseUrl = apiKeys.firebaseConfig.databaseURL;
+
+const getCollectionByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/records.json?orderBy="uid"&equalTo="${uid}"`)
+    .then(({ data }) => resolve(utils.convertFirebaseCollection(data)))
+    .catch((err) => reject(err));
+});
+
+export default {
+  getCollectionByUid,
+};
